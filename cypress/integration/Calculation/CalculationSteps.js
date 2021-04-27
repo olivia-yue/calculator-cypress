efccf1b88c2300f102ca30831e5a84b3b92910cb
yiqun.yue <yiqun.yue@outlook.com>
2021-04-27 16:35:33 +1200

    add workflow file

--- cypress/integration/Calculation/CalculationSteps.js
+++ cypress/integration/Calculation/CalculationSteps.js
@@ -1,18 +1,18 @@
 /// <reference types="cypress" />
 
-import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
-import CalculatorPage from '../../pages/CalculatorPage';
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps"
+import CalculatorPage from '../../pages/CalculatorPage'
 
 const calculatorPage = new CalculatorPage()
 
 Given('I have opened the calculator homepage', () => {
   cy.visit('/').title().should('contains', 'Simple Calculator')
-});
+})
 
 When('I input {} and {} and choose the {} to perform a calculation', (leftNum, rightNum, operator) => {
   cy.log("Performing calculation: " + leftNum + operator + rightNum)
   calculatorPage.doCalculation(leftNum, rightNum, operator)
-});
+})
 
 Then('I should get the expected {}', (result) => {
   calculatorPage.getResult().should('have.value', result)
